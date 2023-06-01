9eacf5fec535c4ceda84c230fcdf255551f37c40
Miguel Lopes <82033110+El1ters@users.noreply.github.com>
2023-06-01 16:40:21 +0100

    Add files via upload

---
+++ Graph/Edge.java
@@ -0,0 +1,6 @@
+package Graph;
+
+public class Edge {
+
+
+}
---
+++ Graph/Graph.java
@@ -0,0 +1,12 @@
+package Graph;
+
+public class Graph {
+    protected int number_nodes;
+    protected int nest_node;
+
+    public Graph(int number_nodes,int nest_node){
+        this.nest_node = nest_node;
+        this.number_nodes = number_nodes;
+    }
+
+}
