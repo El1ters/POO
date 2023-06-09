71133ffd8dbf16e82388601df8465644d6ee766f
Miguel Lopes <82033110+El1ters@users.noreply.github.com>
2023-06-09 13:44:35 +0100

    Delete src/Graph/Graph directory

--- src/Graph/Graph/GI.java
+++
@@ -1,8 +0,0 @@
-package Graph;
-
-import java.util.ArrayList;
-import java.util.List;
-public interface GI {
-    public int get_weight(int i,int j);
-    public List<int[]> get_neighbours(int i);
-}
