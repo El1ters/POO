aaccd20c2f66c45851441d0dcb9f70360347ce9a
laura-da-silva <135249460+laura-da-silva@users.noreply.github.com>
2023-06-15 17:57:02 +0100

    best.java

--- src/ACO/best.java
+++ src/ACO/best.java
@@ -2,6 +2,11 @@ package ACO;
 import java.util.Arrays;
 import java.util.ArrayList;
 
+
+/** ---------------------------------------------------------------------------------------------------------
+ * Best:
+ * Classe que guarda o melhor caminho encontrado e o seu peso
+ ----------------------------------------------------------------------------------------------------------*/
 public class best {
 
     private ArrayList<String> path;
