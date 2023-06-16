c898aadad1dae9b312b9d9b5c65e0440dfb93f97
laura-da-silva <135249460+laura-da-silva@users.noreply.github.com>
2023-06-16 10:28:40 +0100

    best.java

--- src/ACO/best.java
+++ src/ACO/best.java
@@ -2,8 +2,13 @@ package ACO;
 import java.util.Arrays;
 import java.util.ArrayList;
 
+
+/** -----------------------------------------------------------------------------------------------------------------------
+* best:
+* Classe que representa a melhor solução encontrada
+----------------------------------------------------------------------------------------------------------------------- */
 public class best {
 
-    private ArrayList<String> path;
-    private int wsum;
+    private ArrayList<String> path;    //Melhor caminho
+    private int wsum;    //Soma do melhor caminho
 }
