546b6eaba3ff1b020e5a0ec109417d1c65c614f1
laura-da-silva <135249460+laura-da-silva@users.noreply.github.com>
2023-06-16 09:33:02 +0100

    Ant.java

--- src/ACO/Ant.java
+++ src/ACO/Ant.java
@@ -5,15 +5,21 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+
+/** ------------------------------------------------------------------------------------------------------------------
+* Ant:
+* Classe que representa formiga a deslocar-se no grafo
+* A formiga guarda o seu caminho percorrido agenda o seu próximo movimento
+------------------------------------------------------------------------------------------------------------------- */
 public class Ant {
     Graph graph;
     OptimizerACO aco;
     private int ID;
     private ArrayList<Integer> curr_path = new ArrayList<>();    //Caminho atual da formiga
     private ArrayList<float[]> J;
-    private float alpha; //move probability parameter
-    private float beta; //move probability parameter
-    private float delta; //calculate next move wait time parameter
+    private float alpha; //Parametro alpha para o evento do movimento da formiga
+    private float beta; //Parametro beta para o evento do movimento da formiga
+    private float delta; //Parametro delta para o evento do movimento da formiga
     public Ant(int index,float a,float b,float d,Graph graph,OptimizerACO aco){
         this.ID = index;
         this.alpha = a;
@@ -22,8 +28,10 @@ public class Ant {
         this.graph = graph;
         this.aco = aco;
         this.curr_path.add(aco.getNest());  //Considerar 'nest' o ponto inicial do caminho da formiga
-        this.J = new ArrayList<>();
+        this.J = new ArrayList<>();    //Inicializaçnao da lista de feromonas
     }
+
+    // Método que determina o próximo movimento da formiga (seleciona o próximo nó)
     private void move(){
         //Ir buscar o nó atual
         int curr_node = curr_path.get(curr_path.size() - 1);
@@ -49,19 +57,21 @@ public class Ant {
         }
     }
 
-    /* Verifica se a formiga percorreu todos os nós e se regressou ao nó de origem
-    Caso o ciclo de Hamilton tenha sido detetado, retornar 1 */
+    // Método que verifica se a formiga percorreu todos os nós e se regressou ao nó de origem
+    Caso o ciclo de Hamilton tenha sido detetado, retornar 1
     private int hamiltonDetection(){
         int nest = curr_path.get(curr_path.size() - 1);
         return (curr_path.size() == aco.getNodes() + 1 && nest == aco.getNest()) ? 1:0;
     }
+
+    // Método que com base no parâmetro delta e peso da aresta, calcula o tempo (utiliza distribuição exponencial)
     public float calcTime(float delta,int weight){
         float mean = delta * weight;
         Random random = new Random();
         return (float) (-mean * Math.log(1 - random.nextFloat()));
     }
 
-    //Método que atualiza o caminho percorrido pela formiga ao longo do mesmo
+    // Método que atualiza o caminho percorrido pela formiga ao longo do mesmo
     private void updatePath(){
         int next = curr_path.get(curr_path.size() - 1);
         int first = curr_path.indexOf(next);
@@ -73,7 +83,7 @@ public class Ant {
         }
     }
 
-    //Método que indica qual o próximo nó no caminho
+    // Método que indica qual o próximo nó no caminho
     private int next_node(List<float[]> P){
         Random random = new Random();
         float prob = random.nextFloat();    //Gerar um valor aleatório entre 0 e 1 - probabilidade
@@ -85,7 +95,7 @@ public class Ant {
         return 0;
     }
 
-    //Método que seleciona um dos vizinhos do nó através de uma distribuição uniforme
+    // Método que seleciona um dos vizinhos do nó através de uma distribuição uniforme
     private int uniformDist(List<Integer> neighbour){
         Random random = new Random();
         //Escolher um indice random para percorrer a lista e escolher um
@@ -100,7 +110,7 @@ public class Ant {
         return 0;
     }
 
-    //Método calcula a probabilidade comulativa de seleção do nó seguinte com base nos níveis de feromonas
+    // Método calcula a probabilidade comulativa de seleção do nó seguinte com base nos níveis de feromonas
     private List<float[]> getP(float ci){
         List<float[]> P = new ArrayList<>();    //Lista que armazena as probabilidade
         float ptotal;
@@ -157,21 +167,29 @@ public class Ant {
 //            }
 //        }
 //    }
+
+    // Método que atualiza o estado da formiga
     public float update(){
+        //Mover a formiga para o próximo nó
         move();
-        //Chamar deteta hamilton
+        //Chamar deteta hamilton para ver se o caminho já representa um caminho hamiltoniano
         if(hamiltonDetection() == 1){
 //            System.out.println("Hamiltoniano");
             aco.ProcessPath(curr_path);
         }
 //        printPath();
+        //Ir buscar o nó atual e o último visitado no caminho
         int curr_node = curr_path.get(curr_path.size() - 1);
         int last_seen = curr_path.get(curr_path.size() - 2);
+        //Calcula o tempo que a formiga vai ficar no nó atual
         float time = calcTime(this.delta,graph.getWeight(last_seen,curr_node));
+        //Atualiza o caminho removendo os nós entre a primeira e a última ocorrência do nó atual
         updatePath();
 //        printPath();
         return time;
     }
+
+    //Retorna o ID da formiga
     public int getAntID(){
         return this.ID;
     }
